37074a05d9190b63fdd9744209e1b8d63f9a8a92
Matthew Nicholson <matthewn@broadinstitute.org>
2015-09-02 09:57:33 -0400

    flip the ca on/off via configs

--- run.sh
+++ run.sh
@@ -5,18 +5,6 @@
 #Set JAVA_ARGS as passed in
 sed -i "s/JAVA_ARGS=.*/JAVA_ARGS=\"${JAVA_ARGS}\"/g" /etc/sysconfig/puppetserver
 
-#If needed, toggle CA functionality off, otherwise leave default of ON.
-
-if [ $PUPPET_CA != 'true' ]; 
-then 
-  sed -i 's/puppetlabs.services.ca.certificate-authority-service\/certificate-authority-service/#puppetlabs.services.ca.certificate-authority-service\/certificate-authority-service/g' /etc/puppetlabs/puppetserver/bootstrap.cfg;
-fi
-
-
-if [ $PUPPET_CA != 'true' ]; 
-then 
-  sed -i 's/#puppetlabs.services.ca.certificate-authority-disabled-service\/certificate-authority-disabled-service/puppetlabs.services.ca.certificate-authority-disabled-service\/certificate-authority-disabled-service/g' /etc/puppetlabs/puppetserver/bootstrap.cfg;
-fi
 
 #Install gems into the puppetserver as requestes
 for g in $GEMS; do
